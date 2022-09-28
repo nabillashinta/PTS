220e2a7bd8bcfd86c97195a2f9f762e4903fac6c
nabillashinta <111963237+nabillashinta@users.noreply.github.com>
2022-09-28 17:00:42 +0700

    Add files via upload

---
+++ Nabilla_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Rumusan Simpel"<<endl;
+    cout << "Masukkan UTS=";
+    int UTS;
+    cin >> UTS;
+    
+    cout << "Masukkan UAS=";
+    int UAS;
+    cin >> UAS;
+    
+    cout << "Masukka Angka Ketiga=";
+    int angka3;
+    cin >> angka3;
+    
+    
+    int hasil;
+    hasil= (UTS + UAS)/angka3 ;
+   cout << "hasil rumus="<<hasil;
+    
+   
+}
